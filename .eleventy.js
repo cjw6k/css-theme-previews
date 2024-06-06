aa51ae77c6dca815a26c181548d10286bc547f53
cjw6k <cj@w6k.ca>
2024-06-05 22:58:45 -0300

    add matcha.css

--- .eleventy.js
+++ .eleventy.js
@@ -45,6 +45,12 @@ function prepareThemes(eleventyConfig) {
     };
 
     // No-Class
+    eleventyConfig.addPassthroughCopy({'node_modules/@lowlighter/matcha/dist/matcha.lite.css': 'noClassCss/matcha.lite.css'});
+    minifyCss('node_modules/@lowlighter/matcha/dist/matcha.lite.css', 'noClassCss/matcha-lite.min.css');
+    themes.noClassCss.push(
+        {name: 'matcha.css (lite)', css: 'noClassCss/matcha-lite.min.css'},
+    );
+
     eleventyConfig.addPassthroughCopy({'node_modules/water.css/out': 'noClassCss/water.css'});
     themes.noClassCss.push(
         {name: 'water.css', css: 'noClassCss/water.css/water.min.css'},
